b3a9f5c118b997272c869ad8ae230475f5f26754
mkafonso <makaya2afonso@gmail.com>
2020-12-06 16:35:48 -0300

    added styld components + react navigation on mobile

---
+++ mobile/src/screens/SignInScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export default SignInScreen = () => {
+  return (
+    <Container>
+      <Main></Main>
+    </Container>
+  );
+};
+
+const Container = styled.View``;
+
+const Main = styled.View``;
